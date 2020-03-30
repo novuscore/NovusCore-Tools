d70e48bd5c28e1e8eb18c9dbae153389402820ce
Pursche <pursche01@gmail.com>
2020-03-30 23:14:17 +0200

    Add a converter which copies fonts into the data folder

---
+++ converter/Source/Converters/FontCopier/FontCopier.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.Scripting.Hosting;
+
+namespace Converter.Converters
+{
+   
+    class FontConverter : BaseConverter
+    {
+        public override void Init()
+        {
+
+        }
+
+        public override bool CanConvert(string pythonPath, ScriptScope scope, out string error)
+        {
+            error = "";
+            string assetPath = GetAssetPath(pythonPath);
+    
+            if (!assetPath.EndsWith(".ttf"))
+            {
+                error = "We only convert .ttf files";
+                return false;
+            }
+
+            return File.Exists(assetPath);
+        }
+
+        public override bool Convert(string pythonPath, ScriptScope scope, string outputDirectory, out string error)
+        {
+            error = "";
+            string assetPath = GetAssetPath(pythonPath);
+
+            outputDirectory = Path.Combine(outputDirectory);
+
+            int subPathStartIndex = assetPath.IndexOf("client\\Data\\") + "client\\Data\\".Length;
+            string subPath = assetPath.Substring(subPathStartIndex);
+
+            string destinationFilePath = Path.Combine(outputDirectory, subPath);
+            Directory.CreateDirectory(Path.GetDirectoryName(destinationFilePath));
+
+            try
+            {
+                File.Copy(assetPath, destinationFilePath, true);
+                return true;
+            }
+            catch (Exception e)
+            {
+                error = e.Message;
+            }
+
+            return false;
+        }
+    }
+}
