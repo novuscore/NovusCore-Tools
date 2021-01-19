2fda09a85c3f8ca5e1a8fc21ae067463b5d177ee
Pursche <pursche01@gmail.com>
2021-01-19 19:46:05 +0100

    Change which inputs shadercookerstandalone accepts

--- shadercookerstandalone/main.cpp
+++ shadercookerstandalone/main.cpp
@@ -1,6 +1,6 @@
 #include <NovusTypes.h>
 #include <fstream>
-
+#include <filesystem>
 #include <ShaderCooker/ShaderCooker.h>
 #include <Utils/DebugHandler.h>
 
@@ -8,22 +8,36 @@ i32 main(int argc, char* argv[])
 {
     if (argc != 4)
     {
-        NC_LOG_ERROR("Expected three parameters, got %i. Usage: <input> <output> <includeDir>", argc)
+        NC_LOG_ERROR("Expected three parameters, got %i. Usage: <shader_path> <shader_source_dir> <shader_bin_dir>", argc);
+        return -1;
     }
 
-    std::string inputFile = argv[1];
-    std::string outputFile = argv[2];
-    std::string includeDir = argv[3];
+    std::string inputFileName = argv[1];
+    std::string sourceDir = argv[2];
+    std::string binDir = argv[3];
 
+    // Create and init shaderCooker
     ShaderCooker::ShaderCooker shaderCooker;
-    
-    shaderCooker.AddIncludeDir(includeDir);
+    shaderCooker.AddIncludeDir(sourceDir);
 
+    // Get the correct paths
+    std::filesystem::path shaderPath = std::filesystem::path(sourceDir) / inputFileName;
+    shaderPath = std::filesystem::absolute(shaderPath.make_preferred());
+
+    std::string outputFileName = inputFileName + ".spv";
+    std::filesystem::path outputPath = std::filesystem::path(binDir) / outputFileName;
+    outputPath = std::filesystem::absolute(outputPath.make_preferred());
+
+    // Compile shader
     char* blob;
     size_t size;
-    shaderCooker.CompileFile(inputFile, blob, size);
+    shaderCooker.CompileFile(shaderPath, blob, size);
 
-    std::ofstream ofstream(outputFile, std::ios::trunc | std::ofstream::binary);
+    // Create output directories
+    std::filesystem::create_directories(outputPath.parent_path());
+
+    // Output file
+    std::ofstream ofstream(outputPath, std::ios::trunc | std::ofstream::binary);
     ofstream.write(blob, size);
     ofstream.close();
 
