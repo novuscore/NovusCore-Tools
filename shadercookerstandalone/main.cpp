03c2bd1ee8e6b865498b4d489ac962b0f168ad59
Pursche <pursche01@gmail.com>
2021-02-25 23:08:30 +0100

    Use the new way of calling ShaderCooker

--- shadercookerstandalone/main.cpp
+++ shadercookerstandalone/main.cpp
@@ -1,45 +1,95 @@
 #include <NovusTypes.h>
 #include <fstream>
 #include <filesystem>
-#include <ShaderCooker/ShaderCooker.h>
+#include <algorithm>
+#include <execution>
+
+#include <ShaderCooker/ShaderCompiler.h>
+#include <ShaderCooker/ShaderCache.h>
+
 #include <Utils/DebugHandler.h>
+#include <Utils/StringUtils.h>
 
 i32 main(int argc, char* argv[])
 {
-    if (argc != 4)
+    if (argc != 3)
     {
-        DebugHandler::PrintError("Expected three parameters, got %i. Usage: <shader_path> <shader_source_dir> <shader_bin_dir>", argc);
+        DebugHandler::PrintError("Expected two parameters, got %i. Usage: <shader_source_dir> <shader_bin_dir>", argc);
         return -1;
     }
 
-    std::string inputFileName = argv[1];
-    std::string sourceDir = argv[2];
-    std::string binDir = argv[3];
+    std::string sourceDir = argv[1];
+    std::string binDir = argv[2];
+    std::chrono::system_clock::time_point startTime = std::chrono::system_clock::now();
 
-    // Create and init shaderCooker
-    ShaderCooker::ShaderCooker shaderCooker;
-    shaderCooker.AddIncludeDir(sourceDir);
+    std::filesystem::path shaderCachePath = std::filesystem::path(binDir) / "_shaders.cache";
+    shaderCachePath = std::filesystem::absolute(shaderCachePath).make_preferred();
 
-    // Get the correct paths
-    std::filesystem::path shaderPath = std::filesystem::path(sourceDir) / inputFileName;
-    shaderPath = std::filesystem::absolute(shaderPath.make_preferred());
+    ShaderCooker::ShaderCache shaderCache;
 
-    std::string outputFileName = inputFileName + ".spv";
-    std::filesystem::path outputPath = std::filesystem::path(binDir) / outputFileName;
-    outputPath = std::filesystem::absolute(outputPath.make_preferred());
+    const bool debugSkipCache = false;
+    if (!debugSkipCache)
+    {
+        if (shaderCache.Load(shaderCachePath))
+        {
+            DebugHandler::Print("Loaded shadercache from: %s", shaderCachePath.string().c_str());
+        }
+        else
+        {
+            DebugHandler::Print("Creating shadercache at: %s", shaderCachePath.string().c_str());
+        }
+    }
+    else
+    {
+        DebugHandler::Print("Skipped loading shadercache due to debugSkipCache being true");
+    }
 
-    // Compile shader
-    char* blob;
-    size_t size;
-    shaderCooker.CompileFile(shaderPath, blob, size);
+    ShaderCooker::ShaderCompiler compiler;
 
-    // Create output directories
-    std::filesystem::create_directories(outputPath.parent_path());
+    // Find all shader files in the source directory
+    std::vector<std::filesystem::path> shadersToCompile;
+    u32 numNonIncludeShaders = 0;
 
-    // Output file
-    std::ofstream ofstream(outputPath, std::ios::trunc | std::ofstream::binary);
-    ofstream.write(blob, size);
-    ofstream.close();
+    for (auto& dirEntry : std::filesystem::recursive_directory_iterator(sourceDir))
+    {
+        std::filesystem::path path = dirEntry.path();
+        path = path.make_preferred();
 
+        // Skip non files
+        if (!dirEntry.is_regular_file())
+            continue;
+
+        // Skip non .hlsl files
+        if (!StringUtils::EndsWith(path.filename().string(), ".hlsl"))
+            continue;
+
+        if (!StringUtils::EndsWith(path.filename().string(), ".inc.hlsl"))
+            numNonIncludeShaders++;
+
+        // Add this file to our list of shaders to compile
+        shadersToCompile.push_back(path);
+    }
+
+    compiler.SetShaderCache(&shaderCache);
+    compiler.SetSourceDirPath(sourceDir);
+    compiler.SetBinDirPath(binDir);
+    compiler.Start();
+    compiler.AddPaths(shadersToCompile);
+    compiler.Process();
+
+    while (compiler.GetStage() != ShaderCooker::ShaderCompiler::Stage::STOPPED)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    }
+
+    // Save our updated shader cache
+    shaderCache.Save(shaderCachePath);
+
+    std::chrono::system_clock::time_point endTime = std::chrono::system_clock::now();
+    std::chrono::duration<double> duration = endTime - startTime;
+
+    u32 numCompiledShaders = compiler.GetNumCompiledShaders();
+    u32 numSkippedShaders = numNonIncludeShaders - numCompiledShaders;
+    DebugHandler::Print("Compiled %u shaders (%u up to date) in %fs", numCompiledShaders, numSkippedShaders, duration);
     return 0;
 }
